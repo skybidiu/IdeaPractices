200139c63af4d526a25fb189fe88e33c691a1f82
Kaixuan_Xing <Kaixuan_Xing@atplatform.net>
2020-09-25 16:47:54 +0800

    增加一个线程练习包

--- src/com/xingkx/ThreadDemo/SynchronizedTest.java
+++ src/com/xingkx/ThreadDemo/SynchronizedTest.java
@@ -1,7 +1,12 @@
 package com.xingkx.ThreadDemo;
 
+/**
+ * 要点：要能区分好是类对象锁还是实例对象锁便能很灵活的运行synchronized同步锁了
+ * 注意：开销大，可能造成死锁
+ */
 class MyThread implements Runnable{
     private int count;
+    private static int num;
 
     @Override
     public void run() {
@@ -11,6 +16,9 @@ class MyThread implements Runnable{
 
         } else if(str.equals("SyncThread2")){
             syncDecrease();
+        }
+        else{
+            method();
         }
     }
 
@@ -38,16 +46,45 @@ class MyThread implements Runnable{
             }
         }
     }
+    //无论操作的是不是同一个实例对象，只要是该类型，调用该方法，就只有一把锁可用，只允许一条线程执行代码块
+    public void methodSameAsBelow(){
+        synchronized (MyThread.class){
+            for(int i = 0; i < 5; i++) {
+                try {
+                    System.out.println(Thread.currentThread().getName() + ": num = " +(num++));
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public synchronized static void method() {
+        for(int i = 0; i < 5; i++) {
+            try {
+                System.out.println(Thread.currentThread().getName() + ": num = " +(num++));
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
 public class SynchronizedTest {
 
     public static void main(String[] args) {
         MyThread myThread = new MyThread();
 
-        //这里证明了当两条或者多条线程同时访问一个同步方法的时候才会造成阻塞等待状态，可以是不同的同步方法
+        //1.这里证明了当两条或者多条线程同时访问一个同步方法的时候才会造成阻塞等待状态，可以是不同的同步方法
         Thread t1 = new Thread(myThread, "SyncThread1");
         Thread t2 = new Thread(myThread, "SyncThread2");
         t1.start();
         t2.start();
+
+        //2.尽管两条线程操作不同的对象，但是调用的都是静态方法，锁定的也是类对象（类对象锁）
+        Thread t3 = new Thread(new MyThread(), "SyncThread3");
+        Thread t4 = new Thread(new MyThread(), "SyncThread4");
+        t3.start();
+        t4.start();
     }
 }
